c56fb304b698ac4a63643fe6604bfd950c16e5c2
Johnathan Crocker <johnathan.crocker@gmail.com>
2018-07-14 13:55:15 -0500

    Some more work on the battle framework

--- Assets/Scripts/BattleSystem/BattleCore.gd
+++ Assets/Scripts/BattleSystem/BattleCore.gd
@@ -16,9 +16,14 @@ var enemyCurrentMP;
 var playerDamage = 10;
 var enemyDamage = 10;
 
+var enemyhud;
+var playerhud;
+
 func _ready():
 	var player = get_node("Player/PlayerStats");
 	var enemy = get_node("GreenSlime/EnemyStats");
+	playerhud = get_parent().get_node("HUD/PlayerHUD");
+	enemyhud = get_parent().get_node("HUD/EnemyHUD");
 	playerHP = player.hp;
 	playerCurrentHP = playerHP;
 	playerMP = player.mp;
@@ -31,9 +36,28 @@ func _ready():
 	enemyCurrentMP = enemy.mp;
 	enemySP = enemy.sp;
 	enemyCurrentSP = enemy.sp;
+	update_enemy_hud();
+	update_player_hud();
+
+func update_enemy_hud():
+	var hpp = (float(enemyCurrentHP) / float(enemyHP)) * float(100);
+	var spp = (float(enemyCurrentSP) / float(enemySP)) * float(100);
+	var mpp = (float(enemyCurrentMP) / float(enemyMP)) * float(100);
+	enemyhud.get_node("EnemyHPBar").value = hpp
+	enemyhud.get_node("EnemySPBar").value = spp;
+	enemyhud.get_node("EnemyMPBar").value = mpp;
 	
+func update_player_hud():
+	var hpp = (float(playerCurrentHP) / float(playerHP)) * float(100);
+	var spp = (float(playerCurrentSP) / float(playerSP)) * float(100);
+	var mpp = (float(playerCurrentMP) / float(playerMP)) * float(100);
+	playerhud.get_node("PlayerHPBar").value = hpp;
+	playerhud.get_node("PlayerSPBar").value = spp;
+	playerhud.get_node("PlayerMPBar").value = mpp;
+
 func do_player_attack():
 	enemyCurrentHP -= playerDamage;
+	update_enemy_hud();
 	if enemyCurrentHP <= 0:
 		print("You win!");
 	else:
