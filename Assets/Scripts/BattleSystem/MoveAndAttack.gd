98543cadbf20eec61dcc17f546694c6935303a67
Johnathan Crocker <johnathan.crocker@gmail.com>
2018-07-15 19:54:48 -0500

    Updated to version 0.2

--- Assets/Scripts/BattleSystem/MoveAndAttack.gd
+++ Assets/Scripts/BattleSystem/MoveAndAttack.gd
@@ -10,8 +10,10 @@ var inPosition = false;
 
 func _ready():
 	pass
-	
+
+#Moving logic for the battle scene for the player
 func _process(delta):
+	#Move right to get into attack position
 	if isMovingRight:
 		position = Vector2(position.x + (float(180) * delta), position.y);
 		timer += delta;
@@ -19,6 +21,7 @@ func _process(delta):
 			isMovingRight = false;
 			inPosition = true;
 			timer = 0;
+	#Move back to start position
 	elif isMovingLeft:
 		position = Vector2(position.x - (float(180) * delta), position.y);
 		timer += delta;
@@ -26,13 +29,13 @@ func _process(delta):
 			isMovingLeft = false;
 			inStartPosition = true;
 			timer = 0;
-			
+#Called from initiateattack.gd to start the attack sequence
 func move_and_attack():
 	get_node("AnimationPlayer").play("player_attack");
 	isMovingRight = true;
 	inStartPosition = false;
 	inPosition = false;
-	
+
 func move_left():
 	isMovingLeft = true;
 	inPosition = false;
