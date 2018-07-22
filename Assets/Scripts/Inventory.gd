defebee77ed6c6cdd91fe1d3ac16d4f1668d65a0
Johnathan Crocker <johnathan.crocker@gmail.com>
2018-07-22 13:47:00 -0500

    Added methods to inventory for exporting and loading the inventory

--- Assets/Scripts/Inventory.gd
+++ Assets/Scripts/Inventory.gd
@@ -52,6 +52,43 @@ func add_weapon(weapon):
 			weapons[i] = weapon;
 			return;
 	
+func get_portable_inventory():
+	var weps = [];
+	weps.append(weapons[0]);
+	weps.append(weapons[1]);
+	weps.append(weapons[2]);
+	weps.append(weapons[3]);
+	weps.append(weapons[4]);
+	weps.append(weapons[5]);
+	weps.append(weapons[6]);
+	weps.append(weapons[7]);
+	weps.append(weapons[8]);
+	weps.append(weapons[9]);
+	var inv = {
+		"Weapons" : weps,
+		"HealthPotions" : healthPotion,
+		"ManaPotions" : manaPotion,
+		"StaminaPotions" : staminaPotion
+	};
+	return inv;
+	
+func load_portable_inventory(inv):
+	healthPotion = inv["HealthPotions"];
+	manaPotion = inv["ManaPotions"];
+	staminaPotion = inv["StaminaPotions"];
+	
+	weps = inv["Weapons"];
+	weapons[0] = weps[0];
+	weapons[1] = weps[1];
+	weapons[2] = weps[2];
+	weapons[3] = weps[3];
+	weapons[4] = weps[4];
+	weapons[5] = weps[5];
+	weapons[6] = weps[6];
+	weapons[7] = weps[7];
+	weapons[8] = weps[8];
+	weapons[9] = weps[9];
+	
 func has_space_for_weapon():
 	for slot in weapons:
 		if slot == null:
