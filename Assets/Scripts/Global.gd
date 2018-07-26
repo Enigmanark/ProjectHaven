5183952c6afa7bc6968cf1aaf8102ca9a1c76a01
Johnathan Crocker <johnathan.crocker@gmail.com>
2018-07-26 13:59:04 -0500

    Updated forest background

--- Assets/Scripts/Global.gd
+++ Assets/Scripts/Global.gd
@@ -1,7 +1,8 @@
 extends Node
 
 var version = "0.5";
-var server = "http://project-haven.herokuapp.com";
+#var server = "http://project-haven.herokuapp.com";
+var server = "https://project-haven-test.herokuapp.com/";
 #var server = "127.0.0.1";
 var serverPort = 80;
 var networkDelay = 400;
